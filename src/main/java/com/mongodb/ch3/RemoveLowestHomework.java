737a33f6afcec276a4d2341c0889bd967b773e58
renuka.prasad <renuka.prasad@rakuten.com>
2017-02-04 12:57:45 +0900

    Commented my logic and kept mongo documentation logic.

--- src/main/java/com/mongodb/ch3/RemoveLowestHomework.java
+++ src/main/java/com/mongodb/ch3/RemoveLowestHomework.java
@@ -1,13 +1,13 @@
 package com.mongodb.ch3;
 
-import com.mongodb.BasicDBObject;
+import static com.mongodb.client.model.Filters.eq;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import org.bson.Document;
@@ -23,10 +23,31 @@ public class RemoveLowestHomework {
 			while (mongoCursor.hasNext()) {
 				Document doc = mongoCursor.next();
 
-				ArrayList<Document> scores = (ArrayList<Document>) doc.get("scores");
-				Object id = doc.get("_id");
-				BasicDBObject match = new BasicDBObject();
-				match.put( "_id", id );
+				List<Document> scores = (List<Document>) doc.get("scores");
+				// Now find the lowest homework score.
+				Document minScoreObj = null;
+				double minScore = Double.MAX_VALUE;  // Minimum score value.
+
+				for (Document scoreDocument : scores) {
+					// The array contains documents with "type" and "score".
+					double score = scoreDocument.getDouble("score");
+					String type = scoreDocument.getString("type");
+					if (type.equals("homework") && score < minScore) {
+						minScore = score;
+						minScoreObj = scoreDocument; // this is the lowest score obj
+					}
+				}
+				// Remove the lowest score.
+				if (minScoreObj != null) {
+					scores.remove(minScoreObj);   // remove the lowest
+				}
+
+				// replace the scores array for the student
+				mongoCollection.updateOne(eq("_id", doc.get("_id")),
+						new Document("$set", new Document("scores", scores)));
+				/*Object id = doc.get("_id");
+				Document match = mongoCollection.find(Filters.eq("_id", id)).first();
+				//match.put( "_id", id );
 
 				System.out.println(" Id of the document " + id);
 
@@ -41,23 +62,27 @@ public class RemoveLowestHomework {
 				if (homeworkDocuments.size() == 1) {
 					listToUpdate.add(homeworkIterator.next());
 				} else {
-					homeworkIterator.next();
+					Document docToDelete = homeworkIterator.next();
+					System.out.println("Removing  ===> " + docToDelete);
+					mongoCollection.updateOne(match, new Document("$pull", docToDelete));
 					while (homeworkIterator.hasNext()) {
-						listToUpdate.add(homeworkIterator.next());
+						Document docToKeep = homeworkIterator.next();
+						//listToUpdate.add(homeworkIterator.next());
+						System.out.println("Keeping  ===> " + docToKeep);
+						mongoCollection.updateOne(match, new Document("$push", docToKeep));
 					}
-				}
 
-				/*BasicDBObject update = new BasicDBObject();
-				update.put( "$pushAll", listToUpdate );
 
-				mongoCollection.updateOne(match, update);*/
-				System.out.println("Filtered homework documents  ===> " + listToUpdate);
+
+				}*/
+
 				System.out.println("=========================================================");
 			}
 		}
 		finally {
 			mongoCursor.close();
 		}
+		mongoClient.close();
 	}
 
 	private static List<Document> getHomeworkDocuments(Iterator<Document> scoresIterator) {
